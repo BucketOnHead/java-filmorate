bdf118f35d5854cf937fc398c7203e3543e48091
IvanMarakanov <marakanovivan@gmail.com>
2022-10-09 15:38:40 +0300

    add: добавить аннотацию для валидации даты фильма

---
+++ src/main/java/ru/yandex/practicum/filmorate/validation/validator/CinemaBirthdayConstraintValidator.java
@@ -0,0 +1,17 @@
+package ru.yandex.practicum.filmorate.validation.validator;
+
+import lombok.NonNull;
+import ru.yandex.practicum.filmorate.validation.annotation.AfterCinemaBirthday;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class CinemaBirthdayConstraintValidator implements ConstraintValidator<AfterCinemaBirthday, LocalDate> {
+    private static final LocalDate cinemaBirthday = LocalDate.of(1895, 12, 28);
+
+    @Override
+    public boolean isValid(@NonNull LocalDate date, ConstraintValidatorContext cxt) {
+        return date.isAfter(cinemaBirthday);
+    }
+}
