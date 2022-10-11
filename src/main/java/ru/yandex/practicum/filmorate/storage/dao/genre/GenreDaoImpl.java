cda157f87b6bb884a50d3f6e465cc80007b781ed
IvanMarakanov <marakanovivan@gmail.com>
2022-10-11 10:47:48 +0300

    fix: убрать константы с запросами

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/genre/GenreDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/genre/GenreDaoImpl.java
@@ -21,27 +21,6 @@ import static ru.yandex.practicum.filmorate.service.Service.DEPENDENCY_MESSAGE;
 @Slf4j
 @Component
 public class GenreDaoImpl implements GenreDao {
-    private static final String SQL_ADD_FILM_GENRES = ""
-            + "INSERT INTO film_genres (film_id, genre_id) "
-            + "VALUES (?, ?)";
-    private static final String SQL_GET_GENRE = ""
-            + "SELECT genre_id, name "
-            + "FROM genres "
-            + "WHERE genre_id=%d";
-    private static final String SQL_GET_ALL_ORDER_BY_GENRE_ID = ""
-            + "SELECT genre_id, name "
-            + "FROM genres "
-            + "ORDER BY genre_id";
-    private static final String SQL_GET_GENRES_FROM_FILM_ORDER_BY_GENRE = ""
-            + "SELECT f.genre_id, g.name "
-            + "FROM film_genres AS f "
-            + "LEFT OUTER JOIN genres AS g ON f.genre_id = g.genre_id "
-            + "WHERE f.film_id=%d "
-            + "ORDER BY g.genre_id";
-    private static final String SQL_DELETE_FILM_GENRES_WHERE_FILM_ID = ""
-            + "DELETE "
-            + "FROM film_genres "
-            + "WHERE film_id=?";
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -55,7 +34,9 @@ public class GenreDaoImpl implements GenreDao {
     public void add(long filmID, Set<Genre> genres) {
         log.debug("add({}, {})", filmID, genres);
         for (Genre genre : genres) {
-            jdbcTemplate.update(SQL_ADD_FILM_GENRES, filmID, genre.getId());
+            jdbcTemplate.update(""
+                    + "INSERT INTO film_genres (film_id, genre_id) "
+                    + "VALUES (?, ?)", filmID, genre.getId());
             log.trace("Фильму ID_{} добавлен жанр ID_{}.", filmID, genre.getId());
         }
     }
@@ -70,7 +51,10 @@ public class GenreDaoImpl implements GenreDao {
     @Override
     public Genre get(int genreID) {
         log.debug("get({}).", genreID);
-        Genre genre = jdbcTemplate.queryForObject(format(SQL_GET_GENRE, genreID), new GenreMapper());
+        Genre genre = jdbcTemplate.queryForObject(format(""
+                + "SELECT genre_id, name "
+                + "FROM genres "
+                + "WHERE genre_id=%d", genreID), new GenreMapper());
         log.trace("Возвращён жанр: {}.", genre);
         return genre;
     }
@@ -78,7 +62,10 @@ public class GenreDaoImpl implements GenreDao {
     @Override
     public Collection<Genre> getAll() {
         log.debug("getAll().");
-        List<Genre> result = jdbcTemplate.query(SQL_GET_ALL_ORDER_BY_GENRE_ID, new GenreMapper());
+        List<Genre> result = jdbcTemplate.query(""
+                + "SELECT genre_id, name "
+                + "FROM genres "
+                + "ORDER BY genre_id", new GenreMapper());
         log.trace("Возвращенны все жанры: {}.", result);
         return result;
     }
@@ -86,8 +73,12 @@ public class GenreDaoImpl implements GenreDao {
     @Override
     public Set<Genre> getGenres(long filmID) {
         log.debug("getGenres({}).", filmID);
-        Set<Genre> genres = new HashSet<>(jdbcTemplate.query(
-                format(SQL_GET_GENRES_FROM_FILM_ORDER_BY_GENRE, filmID), new GenreMapper()));
+        Set<Genre> genres = new HashSet<>(jdbcTemplate.query(format(""
+                + "SELECT f.genre_id, g.name "
+                + "FROM film_genres AS f "
+                + "LEFT OUTER JOIN genres AS g ON f.genre_id = g.genre_id "
+                + "WHERE f.film_id=%d "
+                + "ORDER BY g.genre_id", filmID), new GenreMapper()));
         log.trace("Возвращены все жанры для фильма ID_{}: {}.", filmID, genres);
         return genres;
     }
@@ -96,7 +87,7 @@ public class GenreDaoImpl implements GenreDao {
     public boolean contains(int genreID) {
         log.debug("contains({}).", genreID);
         try {
-            jdbcTemplate.queryForObject(format(SQL_GET_GENRE, genreID), new GenreMapper());
+            get(genreID);
             log.trace("Найден жанр ID_{}.", genreID);
             return true;
         } catch (EmptyResultDataAccessException ex) {
@@ -107,7 +98,10 @@ public class GenreDaoImpl implements GenreDao {
 
     private void delete(long filmID) {
         log.debug("delete({}).", filmID);
-        jdbcTemplate.update(SQL_DELETE_FILM_GENRES_WHERE_FILM_ID, filmID);
+        jdbcTemplate.update(""
+                + "DELETE "
+                + "FROM film_genres "
+                + "WHERE film_id=?", filmID);
         log.debug("Удалены все жанры у фильма {}.", filmID);
     }
 
