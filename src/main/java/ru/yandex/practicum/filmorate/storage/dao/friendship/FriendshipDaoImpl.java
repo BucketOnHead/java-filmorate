1aeeedfbd17e336c034f9ad3780c6c79fcb23bbf
IvanMarakanov <marakanovivan@gmail.com>
2022-10-11 10:42:22 +0300

    refactor: вынести общую логику в метод get()

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/friendship/FriendshipDaoImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/friendship/FriendshipDaoImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.user.Friendship;
 
@@ -19,6 +20,7 @@ import static ru.yandex.practicum.filmorate.service.Service.DEPENDENCY_MESSAGE;
 @Slf4j
 @Component
 public class FriendshipDaoImpl implements FriendshipDao {
+    private static final RowMapper<Friendship> friendshipMapper = new BeanPropertyRowMapper<>(Friendship.class);
     private final JdbcTemplate jdbcTemplate;
 
     @Autowired
@@ -34,24 +36,14 @@ public class FriendshipDaoImpl implements FriendshipDao {
         jdbcTemplate.update(""
                 + "INSERT INTO friendships (from_user_id, to_user_id, isMutual) "
                 + "VALUES(?, ?, ?)", fromUserID, toUserID, isMutual);
-        Friendship result = jdbcTemplate.queryForObject(format(""
-                        + "SELECT from_user_id, to_user_id, isMutual "
-                        + "FROM friendships "
-                        + "WHERE from_user_id=%d "
-                        + "AND to_user_id=%d", fromUserID, toUserID),
-                new BeanPropertyRowMapper<>(Friendship.class));
+        Friendship result = get(fromUserID, toUserID);
         log.trace("Добавлена связь: {}.", result);
     }
 
     @Override
     public void delete(long fromUserID, long toUserID) {
         log.debug("delete({}, {}).", fromUserID, toUserID);
-        Friendship result = Objects.requireNonNull(jdbcTemplate.queryForObject(format(""
-                        + "SELECT from_user_id, to_user_id, isMutual "
-                        + "FROM friendships "
-                        + "WHERE from_user_id=%d "
-                        + "AND to_user_id=%d", fromUserID, toUserID),
-                new BeanPropertyRowMapper<>(Friendship.class)));
+        Friendship result = Objects.requireNonNull(get(fromUserID, toUserID));
         jdbcTemplate.update(""
                 + "DELETE FROM friendships "
                 + "WHERE from_user_id=? "
@@ -62,7 +54,8 @@ public class FriendshipDaoImpl implements FriendshipDao {
                     + "SET isMutual=false "
                     + "WHERE from_user_id=? "
                     + "AND to_user_id=?", toUserID, fromUserID);
-            log.debug("Дружба между {} и {} перестала быть взаимной.", toUserID, fromUserID);
+            log.debug("Дружба между {} и {} перестала быть взаимной.",
+                    toUserID, fromUserID);
         }
         log.trace("Удалена связь: {}.", result);
     }
@@ -71,13 +64,14 @@ public class FriendshipDaoImpl implements FriendshipDao {
     public Collection<Long> getFromUserID(long toUserId) {
         log.debug("getFriendships({}).", toUserId);
         List<Long> friendships = jdbcTemplate.query(format(""
-                                + "SELECT from_user_id, to_user_id, IsMutual "
-                                + "FROM friendships "
-                                + "WHERE to_user_id=%d", toUserId),
-                        new BeanPropertyRowMapper<>(Friendship.class)).stream()
+                        + "SELECT from_user_id, to_user_id, IsMutual "
+                        + "FROM friendships "
+                        + "WHERE to_user_id=%d", toUserId), friendshipMapper)
+                .stream()
                 .map(Friendship::getFromUserId)
                 .collect(Collectors.toList());
-        log.trace("Возврашены запросы на дружбу с пользователем ID_{}: {}.", toUserId, friendships);
+        log.trace("Возврашены запросы на дружбу с пользователем ID_{}: {}.",
+                toUserId, friendships);
         return friendships;
     }
 
@@ -85,12 +79,7 @@ public class FriendshipDaoImpl implements FriendshipDao {
     public boolean contains(long fromUserID, long toUserID) {
         log.debug("contains({}, {}).", fromUserID, toUserID);
         try {
-            jdbcTemplate.queryForObject(format(""
-                            + "SELECT from_user_id, to_user_id, isMutual "
-                            + "FROM friendships "
-                            + "WHERE from_user_id=%d "
-                            + "AND to_user_id=%d", fromUserID, toUserID),
-                    new BeanPropertyRowMapper<>(Friendship.class));
+            get(fromUserID, toUserID);
             log.trace("Найден запрос на дружбу от пользователя ID_{} к пользователю ID_{}.",
                     fromUserID, toUserID);
             return true;
@@ -99,5 +88,23 @@ public class FriendshipDaoImpl implements FriendshipDao {
                     fromUserID, toUserID);
             return false;
         }
+    }
+
+    /**
+     * Метод возвращает связь между пользоватедями
+     *
+     * @param fromUserID идентификатор пользователя,
+     *                   отправившего запрос на дружбу.
+     * @param toUserID   идентификатор пользователя,
+     *                   которому был отправлен запрос
+     *                   на дружбу.
+     * @return Связь между пользователями.
+     */
+    private Friendship get(long fromUserID, long toUserID) {
+        return jdbcTemplate.queryForObject(format(""
+                + "SELECT from_user_id, to_user_id, isMutual "
+                + "FROM friendships "
+                + "WHERE from_user_id=%d "
+                + "AND to_user_id=%d", fromUserID, toUserID), friendshipMapper);
     }
 }
