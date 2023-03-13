6f3ffb5541594319255fd1944b762ecbdcfe1688
IvanMarakanov <marakanovivan@gmail.com>
2023-03-13 09:19:48 +0300

    refactor: controller advice

--- src/main/java/ru/yandex/practicum/filmorate/service/Service.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/Service.java
@@ -3,15 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import ru.yandex.practicum.filmorate.storage.Storage;
 
 public interface Service<T> extends Storage<T> {
-    /**
-     * [Exception].getClass().getSimpleName(), [exception].getMessage().
-     */
-    String EXCEPTION_MESSAGE = "Отловлена ошибка: {}({}).";
-
-    /**
-     * [dependency].getClass().getName().
-     */
-    String DEPENDENCY_MESSAGE = "Подключена зависимость: {}.";
 
     @Override
     default boolean contains(long elementID) {
