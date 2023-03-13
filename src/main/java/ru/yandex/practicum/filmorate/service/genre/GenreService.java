8dc73729cd38052f3183a628f2a4aedf6d06dfad
IvanMarakanov <marakanovivan@gmail.com>
2023-03-13 09:33:16 +0300

    refactor: genre service

--- src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/genre/GenreService.java
@@ -4,6 +4,7 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.Service;
 
 public interface GenreService extends Service<Genre> {
+
     @Override
     default Genre add(Genre element) {
         throw new UnsupportedOperationException("Используйте data.sql чтобы добавить новые жанры");
